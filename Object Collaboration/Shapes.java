7a20851fd7a27218da1bfd9c71afb0d4cf29c91b
mohitbirla20 <106754547+mohitbirla20@users.noreply.github.com>
2022-08-06 22:33:17 +0530

    Add files via upload

---
+++ Object Collaboration/Shapes.java
@@ -0,0 +1,20 @@
+package findArea;
+
+public class Shapes {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+	Circle findAns = new Circle();
+	findAns.area(4);
+		
+	Rectangle findAnsR = new Rectangle();
+	findAnsR.area(4,7);
+	
+	Square findAnsS = new Square();
+	findAnsS.area(6);
+	}
+
+}
+
