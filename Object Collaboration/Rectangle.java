e1495c053333b5dc27fa05a44f52c508f771c1fe
mohitbirla20 <106754547+mohitbirla20@users.noreply.github.com>
2022-08-06 22:32:58 +0530

    Add files via upload

---
+++ Object Collaboration/Rectangle.java
@@ -0,0 +1,14 @@
+package findArea;
+
+public class Rectangle {
+
+	int length ;
+	int breadth;
+	
+	public void area(int length , int breadth) {
+		
+		System.out.println(length*breadth);
+		
+	}
+	
+}
