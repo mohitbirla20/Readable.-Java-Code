b636c94aaf15b71e7a2883a0a8e45175c5048ac9
mohitbirla20 <106754547+mohitbirla20@users.noreply.github.com>
2022-08-08 10:54:36 +0530

    A class check method

---
+++ Object Collaboration/A.java
@@ -0,0 +1,43 @@
+package evalutaionn;
+
+public class A {
+
+      int x = 10;
+      
+      void showNumber(String username,String Password) {
+    	  if(username == "admin" && Password == "123") {
+    		  System.out.println("Login Succes");
+    	  }else {
+    		  System.out.println("Wrong credatial");
+    	  }
+      }
+
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
