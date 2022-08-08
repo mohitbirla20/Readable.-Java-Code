72c4078108677a8156f78f29ffff98102c7f461f
mohitbirla20 <106754547+mohitbirla20@users.noreply.github.com>
2022-08-08 10:56:03 +0530

    main class

---
+++ Object Collaboration/Private.java
@@ -0,0 +1,26 @@
+package evalutaionn;
+
+public class Private {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       B obj = new B();
+      
+       obj.setAcc(46545, "mohit birla");
+       int acc = obj.getAcc();
+       double accBalence = obj.getBalance();
+       
+       System.out.println(accBalence);
+	  System.out.println(acc);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
