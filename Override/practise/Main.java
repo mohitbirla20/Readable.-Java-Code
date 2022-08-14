2246f7dfb0f875e9365d6aeacdf7fc68f5826d72
mohitbirla20 <106754547+mohitbirla20@users.noreply.github.com>
2022-08-14 19:34:06 +0530

    main class

---
+++ Override/practise/Main.java
@@ -0,0 +1,15 @@
+package practise.classnotes;
+
+public class Main {
+
+	
+	
+	Main(){
+	System.out.println("styam");
+	}
+	public static void main(String[] args){
+     Main a = new Main();
+	}
+	
+
+}
