e0a76a6dd5f735bed4f77038e73a9ee90533252c
mohitbirla20 <106754547+mohitbirla20@users.noreply.github.com>
2022-08-14 19:31:19 +0530

    override

---
+++ Override/Person.java
@@ -0,0 +1,17 @@
+package unit_4.sp3d3;
+
+public class Person {
+     String name;
+     String gender;
+     Address add;
+     
+     
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", gender=" + gender + ", add=" + add + "]";
+	}
+     
+	
+     
+     
+}
