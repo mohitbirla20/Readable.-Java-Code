8be912ee794cc872b525677d9337bfdbe9729a62
mohitbirla20 <106754547+mohitbirla20@users.noreply.github.com>
2022-08-14 19:30:17 +0530

    overrding

---
+++ Override/Instructor.java
@@ -0,0 +1,15 @@
+package unit_4.sp3d3;
+
+public class Instructor extends Person{
+
+  int instructorId;
+  int salary ;
+  
+@Override
+public String toString() {
+	return "Instructor [instructorId=" + instructorId + ", salary=" + salary + "]";
+}
+
+  
+	
+}
