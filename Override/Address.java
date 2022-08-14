01cc1822b7f526c6c6a66411d7c3dcd6a30d049a
mohitbirla20 <106754547+mohitbirla20@users.noreply.github.com>
2022-08-14 19:29:47 +0530

    overrding

---
+++ Override/Address.java
@@ -0,0 +1,17 @@
+package unit_4.sp3d3;
+
+public class Address {
+
+	String city;
+	String state;
+	String pinCode;
+	
+	
+	@Override
+	public String toString() {
+		return "Address [city=" + city + ", state=" + state + ", pinCode=" + pinCode + "]";
+	}
+	
+	
+	
+}
