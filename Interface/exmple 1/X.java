ce9436e5bdfe57e09b315df1e9b05976fdd00952
mohitbirla20 <106754547+mohitbirla20@users.noreply.github.com>
2022-08-15 21:31:35 +0530

    interface

---
+++ Interface/exmple 1/X.java
@@ -0,0 +1,14 @@
+package sp3d5.assginments;
+
+public interface X {
+    
+	public abstract void sum();
+	
+	void sum2();
+	
+	public static void Xinterface() {
+		System.out.println("Inside Xinterface");
+	}
+}
+
+
