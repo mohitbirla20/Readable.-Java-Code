33bb9d222f95a807752373b7ac9e34586c2ae3c0
mohitbirla20 <106754547+mohitbirla20@users.noreply.github.com>
2022-08-15 21:34:38 +0530

    implements

---
+++ Interface/exmple 2/RoadSideHotel.java
@@ -0,0 +1,17 @@
+package sp3d5.question2;
+
+public class RoadSideHotel implements Hotel{
+
+	@Override
+	public void chickenBiryani() {
+		// TODO Auto-generated method stub
+		System.out.println("Chicken biryani from road side Hotel");
+	}
+
+	@Override
+	public void masalaDosa() {
+		// TODO Auto-generated method stub
+		System.out.println("masal dosa from road side Hotel");
+	}
+
+}
