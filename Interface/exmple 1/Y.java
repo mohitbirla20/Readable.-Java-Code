c32ddad20a17e06824775bb2608bd6546baa8e01
mohitbirla20 <106754547+mohitbirla20@users.noreply.github.com>
2022-08-15 21:32:05 +0530

    interface

---
+++ Interface/exmple 1/Y.java
@@ -0,0 +1,13 @@
+package sp3d5.assginments;
+
+public interface Y {
+
+public abstract void ysum();
+	
+	void ysum2();
+	
+	public static void Xinterface() {
+		System.out.println("Inside Yinterface");
+	}
+}
+	
