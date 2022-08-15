a1b4ec80b4cc0aed63103f091e887d910ef96501
mohitbirla20 <106754547+mohitbirla20@users.noreply.github.com>
2022-08-15 21:34:55 +0530

    implements

---
+++ Interface/exmple 2/TajHotel.java
@@ -0,0 +1,21 @@
+package sp3d5.question2;
+
+public class TajHotel implements Hotel{
+
+	@Override
+	public void chickenBiryani() {
+		// TODO Auto-generated method stub
+		System.out.println("Chicken biryani from tajHotel");
+	}
+
+	@Override
+	public void masalaDosa() {
+		// TODO Auto-generated method stub
+		System.out.println("masal Dosa from tajHotel");
+	}
+
+	public void welcomeDrink(){
+		System.out.println("Welcome Drink from the TajHotel");
+		}
+	
+}
