b15c937252e1ed7ad1b02fe26bc0de7fd926624b
mohitbirla20 <106754547+mohitbirla20@users.noreply.github.com>
2022-09-16 11:03:05 +0530

    besic multithreading program

---
+++ Multithreading & Executor Framework/multitreading/ThreadA.java
@@ -0,0 +1,14 @@
+package multitreading;
+
+public class ThreadA extends Thread{
+    
+	@Override
+	public void run() {
+		
+		for(int i=0;i<10;i++) {
+			System.out.println(i+" "+Thread.currentThread().getName());
+		}
+		
+	}
+	
+}
